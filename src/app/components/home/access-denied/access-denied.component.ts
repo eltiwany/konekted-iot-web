bc54e16c1f6fe929c6b9c926a494299a1ec214b0
eltiwany <alisaleh42797@gmail.com>
2022-01-22 23:56:11 +0300

    Generated Access Denied Component

---
+++ src/app/components/home/access-denied/access-denied.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-access-denied',
+  templateUrl: './access-denied.component.html',
+  styleUrls: ['./access-denied.component.sass']
+})
+export class AccessDeniedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
