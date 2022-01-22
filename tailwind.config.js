9633d585ddd5610b0cae6111bce7bce9a625936a
eltiwany <alisaleh42797@gmail.com>
2022-01-22 23:12:44 +0300

    Added Tailwind and B-Icons Support

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
+  },
+  // prefix: 'tw',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
