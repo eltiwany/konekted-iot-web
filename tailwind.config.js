5be1ac85e43548e7167cb8485a2a918c2982b436
eltiwany <alisaleh42797@gmail.com>
2022-01-23 19:58:20 +0300

    Remove JIT on tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  mode: 'jit',
   purge: {
     enabled: true,
-    content: ['./src/**/*.{html,ts}']
+    content: [
+      './src/**/*.{html,ts}'
+    ]
   },
   // prefix: 'tw',
   content: [],
